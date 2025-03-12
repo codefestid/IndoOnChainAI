d097894d2e047436cb036415af7be2790d59365a
CodeFest Indonesia <162551779+codefestid@users.noreply.github.com>
2025-03-12 11:22:16 +0700

    Add files via upload

---
+++ src/IndoOnChainAI_frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+// import { IndoOnChainAI_backend } from 'declarations/IndoOnChainAI_backend';
+
+function App() {
+  return (
+
+    <iframe
+      src="https://indonesiaonchain.com/"
+      style={{ width: '100%', height: '100vh', border: 'none', padding: "0" }}
+      title="External Website"
+    />
+
+  );
+}
+
+export default App;
